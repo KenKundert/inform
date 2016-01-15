3b8dd82ec008186cd79ef3a993e7a806ea81f55c
Ken Kundert <thenurd@nurdletech.com>
2016-01-14 23:56:50 -0800

    Adjust version compatibility guides.

--- setup.py
+++ setup.py
@@ -33,8 +33,10 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Utilities',
     ],
 )
