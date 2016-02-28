e5777d633f4919858e9833425fcf6d2ec627f15f
Ken Kundert <thenurd@nurdletech.com>
2016-02-27 16:44:56 -0800

    Bump version: 1.3.2 → 1.3.3

--- inform/__init__.py
+++ inform/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.3.2'
+__version__ = '1.3.3'
 
 from .inform import (
     indent, cull, is_str, is_iterable, is_collection,
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='inform',
-    version='1.3.2',
+    version='1.3.3',
     description='print & logging utilities for communicating with user',
     long_description=readme,
     author="Ken Kundert",
