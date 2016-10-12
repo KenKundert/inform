ed671796582370d3957daec6dff8dd844573c4f0
Ken Kundert <thenurd@nurdletech.com>
2016-10-12 13:03:40 -0700

    Bump version: 1.4.3 → 1.4.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='inform',
-    version='1.4.3',
+    version='1.4.4',
     description='print & logging utilities for communicating with user',
     long_description=readme,
     author="Ken Kundert",
