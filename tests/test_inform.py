7ddd89cd3855e8e3fb6508959044bf6c21226db9
Ken Kundert <ken@theKunderts.net>
2017-03-20 09:43:51 -0700

    Improve tests.

--- tests/test_inform.py
+++ tests/test_inform.py
@@ -1,7 +1,8 @@
 from inform import (
-    Inform, Error, display, done, error, errors_accrued, log, output, terminate,
-    terminate_if_errors, warn
+    Inform, Error, codicil, display, done, error, errors_accrued, log, output,
+    terminate, terminate_if_errors, warn
 )
+from textwrap import dedent
 import sys
 
 if sys.version[0] == '2':
@@ -45,6 +46,7 @@ def test_display():
          Inform(stdout=stdout, stderr=stderr, logfile=logfile) as msg \
     :
         display('hey now!', culprit=('yo', 'ho'))
+        display('yep,\nYEP!', culprit='yep yep yep yep yep yep yep yep yep yep yep')
 
         num_errors = msg.errors_accrued()
         num_errors2 = errors_accrued()
@@ -54,13 +56,21 @@ def test_display():
 
     assert num_errors == 0
     assert num_errors2 == 0
-    assert str(output_text) == 'yo, ho: hey now!\n'
+    assert str(output_text) == dedent('''
+        yo, ho: hey now!
+        yep yep yep yep yep yep yep yep yep yep yep:
+            yep,
+                YEP!
+    ''').lstrip()
     assert str(error_text) == ''
     log_lines = logfile_text.split('\n')
-    assert len(log_lines) == 3
+    assert len(log_lines) == 6
     assert log_lines[0].startswith('Invoked as')
     assert log_lines[1] == 'yo, ho: hey now!'
-    assert log_lines[2] == ''
+    assert log_lines[2] == 'yep yep yep yep yep yep yep yep yep yep yep:'
+    assert log_lines[3] == '    yep,'
+    assert log_lines[4] == '        YEP!'
+    assert log_lines[5] == ''
 
 def test_output():
     with StringIO() as stdout, \
@@ -69,6 +79,8 @@ def test_output():
          Inform(stdout=stdout, stderr=stderr, logfile=logfile) as msg \
     :
         output('hey now!')
+        codicil('baby', 'bird', sep='\n')
+        msg.flush_logfile()
 
         num_errors = msg.errors_accrued()
         num_errors2 = errors_accrued()
@@ -78,13 +90,15 @@ def test_output():
 
     assert num_errors == 0
     assert num_errors2 == 0
-    assert str(output_text) == 'hey now!\n'
+    assert str(output_text) == 'hey now!\nbaby\nbird\n'
     assert str(error_text) == ''
     log_lines = logfile_text.split('\n')
-    assert len(log_lines) == 3
+    assert len(log_lines) == 5
     assert log_lines[0].startswith('Invoked as')
     assert log_lines[1] == 'hey now!'
-    assert log_lines[2] == ''
+    assert log_lines[2] == 'baby'
+    assert log_lines[3] == 'bird'
+    assert log_lines[4] == ''
 
     try:
         terminate_if_errors()
@@ -96,9 +110,14 @@ def test_error():
     with StringIO() as stdout, \
          StringIO() as stderr, \
          StringIO() as logfile, \
-         Inform(stdout=stdout, stderr=stderr, logfile=logfile, prog_name=False) as msg \
+         Inform(
+            stdout=stdout, stderr=stderr, logfile=logfile, prog_name=False,
+            hanging_indent=False
+         ) as msg \
     :
         error('hey now!')
+        codicil('baby', 'bird', sep='\n')
+        error('yep,\nYEP!', culprit='yep yep yep yep yep yep yep yep yep yep yep')
 
         num_errors = msg.errors_accrued()
         num_errors2 = errors_accrued(True)
@@ -107,24 +126,42 @@ def test_error():
         error_text = stderr.getvalue()
         logfile_text = logfile.getvalue()
 
-    assert num_errors == 1
-    assert num_errors2 == 1
+    assert num_errors == 2
+    assert num_errors2 == 2
     assert num_errors3 == 0
-    assert str(output_text) == 'error: hey now!\n'
+    assert str(output_text) == dedent('''
+        error: hey now!
+            baby
+            bird
+        error:
+            yep yep yep yep yep yep yep yep yep yep yep:
+                yep,
+                YEP!
+    ''').lstrip()
     assert str(error_text) == ''
     log_lines = logfile_text.split('\n')
-    assert len(log_lines) == 3
+    assert len(log_lines) == 9
     assert log_lines[0].startswith('Invoked as')
     assert log_lines[1] == 'error: hey now!'
-    assert log_lines[2] == ''
+    assert log_lines[2] == '    baby'
+    assert log_lines[3] == '    bird'
+    assert log_lines[4] == 'error:'
+    assert log_lines[5] == '    yep yep yep yep yep yep yep yep yep yep yep:'
+    assert log_lines[6] == '        yep,'
+    assert log_lines[7] == '        YEP!'
+    assert log_lines[8] == ''
 
 def test_warn():
     with StringIO() as stdout, \
          StringIO() as stderr, \
          StringIO() as logfile, \
-         Inform(stdout=stdout, stderr=stderr, logfile=logfile, prog_name=False) as msg \
+         Inform(
+            stdout=stdout, stderr=stderr, logfile=logfile, prog_name=False
+        ) as msg \
     :
         warn('hey now!', culprit='yo')
+        codicil('baby', 'bird', sep='\n')
+        warn('yep,\nYEP!', culprit='yep yep yep yep yep yep yep yep yep yep yep')
 
         num_errors = msg.errors_accrued()
         num_errors2 = errors_accrued()
@@ -134,13 +171,27 @@ def test_warn():
 
     assert num_errors == 0
     assert num_errors2 == 0
-    assert str(output_text) == 'warning: yo: hey now!\n'
+    assert str(output_text) == dedent('''
+        warning: yo: hey now!
+            baby
+                bird
+        warning:
+            yep yep yep yep yep yep yep yep yep yep yep:
+                yep,
+                    YEP!
+    ''').lstrip()
     assert str(error_text) == ''
     log_lines = logfile_text.split('\n')
-    assert len(log_lines) == 3
+    assert len(log_lines) == 9
     assert log_lines[0].startswith('Invoked as')
     assert log_lines[1] == 'warning: yo: hey now!'
-    assert log_lines[2] == ''
+    assert log_lines[2] == '    baby'
+    assert log_lines[3] == '        bird'
+    assert log_lines[4] == 'warning:'
+    assert log_lines[5] == '    yep yep yep yep yep yep yep yep yep yep yep:'
+    assert log_lines[6] == '        yep,'
+    assert log_lines[7] == '            YEP!'
+    assert log_lines[8] == ''
 
 def test_Error():
     try:
