3b1f37c436dda2c387784ec1ec330318d8b3a31d
Ken Kundert <ken@theKunderts.net>
2018-12-03 18:47:30 -0800

    Fix tests.

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # Test Inform debug functions
 
 try:                 # python3
@@ -18,7 +16,7 @@ def test_anglicize(capsys):
     ppp()
     out, err = capsys.readouterr()
     assert out == dedent('''
-        DEBUG: test_debug.py, 18, test_debug.test_anglicize()
+        DEBUG: test_debug.py, 16, test_debug.test_anglicize()
     ''').lstrip()
 
 def test_grouch(capsys):
@@ -28,7 +26,7 @@ def test_grouch(capsys):
     ppp('hey now!', a, b)
     out, err = capsys.readouterr()
     assert out == dedent('''
-        DEBUG: test_debug.py, 28, test_debug.test_grouch(): hey now! 0 b
+        DEBUG: test_debug.py, 26, test_debug.test_grouch(): hey now! 0 b
     ''').lstrip()
 
 def test_salver(capsys):
@@ -41,7 +39,7 @@ def test_salver(capsys):
     ddd('hey now!', a, b, c, d, e)
     out, err = capsys.readouterr()
     assert out == dedent('''
-        DEBUG: test_debug.py, 41, test_debug.test_salver():
+        DEBUG: test_debug.py, 39, test_debug.test_salver():
             'hey now!'
             0
             'b'
@@ -60,7 +58,7 @@ def test_daiquiri(capsys):
     ddd(s='hey now!', a=a, b=b, c=c, d=d, e=e)
     out, err = capsys.readouterr()
     assert out == dedent('''
-        DEBUG: test_debug.py, 60, test_debug.test_daiquiri():
+        DEBUG: test_debug.py, 58, test_debug.test_daiquiri():
             a = 0
             b = 'b'
             c = [0, 'b']
@@ -79,7 +77,7 @@ def test_prude(capsys):
     Info(email='ted@ledbelly.com')
     out, err = capsys.readouterr()
     assert out == dedent('''
-        DEBUG: test_debug.py, 75, test_debug.Info.__init__():
+        DEBUG: test_debug.py, 73, test_debug.Info.__init__():
             email = 'ted@ledbelly.com'
             self = Info object containing {'email': 'ted@ledbelly.com'}
     ''').lstrip()
@@ -95,7 +93,7 @@ def test_update(capsys):
     out, err = capsys.readouterr()
     out = '\n'.join(l for l in out.split('\n') if 'capsys' not in l)
     assert out == dedent('''
-        DEBUG: test_debug.py, 94, test_debug.test_update():
+        DEBUG: test_debug.py, 92, test_debug.test_update():
             a = 0
             b = 'b'
             c = [0, 'b']
@@ -113,7 +111,7 @@ def test_shear(capsys):
     vvv(a, b, c, d, e)
     out, err = capsys.readouterr()
     assert out == dedent('''
-        DEBUG: test_debug.py, 113, test_debug.test_shear():
+        DEBUG: test_debug.py, 111, test_debug.test_shear():
             a = 0
             b = 'b'
             c = [0, 'b']
@@ -126,8 +124,8 @@ def test_prostrate(capsys):
     sss()
     out, err = capsys.readouterr()
     out = out.strip().split('\n')
-    assert out[0] == 'DEBUG: test_debug.py, 126, test_debug.test_prostrate():'
-    assert out[-2][-57:] == "inform/tests/test_debug.py', line 126, in test_prostrate,"
+    assert out[0] == 'DEBUG: test_debug.py, 124, test_debug.test_prostrate():'
+    assert out[-2][-57:] == "inform/tests/test_debug.py', line 124, in test_prostrate,"
     assert out[-1] == '        sss()'
 
 def test_rubber(capsys):
@@ -135,14 +133,14 @@ def test_rubber(capsys):
     a = aaa('a')
     out, err = capsys.readouterr()
     assert out == dedent('''
-        DEBUG: test_debug.py, 135, test_debug.test_rubber(): 'a'
+        DEBUG: test_debug.py, 133, test_debug.test_rubber(): 'a'
     ''').lstrip()
     assert a == 'a'
 
     b = aaa(b = 'b')
     out, err = capsys.readouterr()
     assert out == dedent('''
-        DEBUG: test_debug.py, 142, test_debug.test_rubber(): b: 'b'
+        DEBUG: test_debug.py, 140, test_debug.test_rubber(): b: 'b'
     ''').lstrip()
     assert b == 'b'
 
--- tests/test_inform.py
+++ tests/test_inform.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 # Test Inform
 
 # Imports {{{1
