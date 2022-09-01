ab96b173aebbd11cdc667c98ad16c5408a3c4eaf
Ken Kundert <ken@theKunderts.net>
2022-08-31 22:06:29 -0700

    deprecate pytest-runner

--- setup.py
+++ setup.py
@@ -22,8 +22,6 @@ setup(
     download_url = 'https://github.com/kenkundert/inform/tarball/master',
     packages = 'inform'.split(),
     install_requires = dependencies.split(),
-    setup_requires = 'pytest-runner>=2.0'.split(),
-    tests_require = 'pytest pytest-cov hypothesis'.split(),
     python_requires = '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*',
     zip_safe = True,
     license = 'GPLv3+',
