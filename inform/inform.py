1d0c238001124e188996c217324f1b8e28d6d6b8
Ken Kundert <ken@theKunderts.net>
2023-05-26 14:22:17 -0700

    fix the license

--- inform/inform.py
+++ inform/inform.py
@@ -2,24 +2,10 @@
 # encoding: utf8
 #
 # Utilities for communicating directly with the user.
-#
 # Documentation can be found at inform.readthedocs.io.
-
-# License {{{1
+#
 # Copyright (c) 2014-2023 Kenneth S. Kundert
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation, either version 3 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program.  If not, see http://www.gnu.org/licenses/.
+# This software is licensed under the `MIT Licents <https://mit-license.org>`_.
 
 # Imports {{{1
 import arrow
