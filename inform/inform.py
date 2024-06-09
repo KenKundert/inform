a76aaaaa0ad65e2516b61cb9f5c905fccd5fca0d
Ken Kundert <ken@theKunderts.net>
2024-06-08 23:13:27 -0700

    update test dependencies in pyproject.toml

--- inform/inform.py
+++ inform/inform.py
@@ -3040,8 +3040,8 @@ class Inform:
             ...    lines = f.read().splitlines()
             ...    num_lines = count_lines(lines)
             warning: pyproject.toml, 25: empty line.
-            warning: pyproject.toml, 33: empty line.
-            warning: pyproject.toml, 39: empty line.
+            warning: pyproject.toml, 37: empty line.
+            warning: pyproject.toml, 43: empty line.
 
         """
         return self.CulpritContextManager(self, culprit, append=False)
